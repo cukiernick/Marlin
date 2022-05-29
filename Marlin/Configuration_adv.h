2da662df41ff5eb9f18f25e5d674dbaf1dde7881
petrzmax <petrzmax@gmail.com>
2022-05-29 15:07:46 +0200

    Fix merge failure

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2996,7 +2996,7 @@
    * Set *_SERIAL_TX_PIN and *_SERIAL_RX_PIN to match for all drivers
    * on the same serial port, either here or in your board's pins file.
    */
-  //#define  X_SLAVE_ADDRESS 0
+  #define  X_SLAVE_ADDRESS 0
   #define  Y_SLAVE_ADDRESS 1
   #define  Z_SLAVE_ADDRESS 2
   //#define X2_SLAVE_ADDRESS 0
